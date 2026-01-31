f0c4c629b4a6d627a489c6b1f516bd8674a088d4
Sīmanis Mikoss <simanism@inbox.lv>
2026-01-31 11:43:23 +0200

    mask interact

--- addons/proto_controller/camera_3d.gd
+++ addons/proto_controller/camera_3d.gd
@@ -1,28 +1,36 @@
 extends Camera3D
 
 @export var ray_length := 1000.0
+var has_valid_interactable: bool = false
+var interactable
 
 func _physics_process(_delta):
+	check_for_interactable()
 	if Input.is_action_just_pressed("interact"):
 		interact()
 
-func interact():
+func check_for_interactable():
 	var mouse_pos = get_viewport().get_mouse_position()
 	var origin = project_ray_origin(mouse_pos)
 	var direction = project_ray_normal(mouse_pos)
-
 	var query = PhysicsRayQueryParameters3D.create(
 		origin,
 		origin + direction * ray_length
 	)
-
 	query.collide_with_areas = true
-
 	var result = get_world_3d().direct_space_state.intersect_ray(query)
 
 	if result:
 		var collider = result.collider
-		print("Hit:", collider.name)
-
 		if collider.has_method("interact"):
-			collider.interact(self)
+			has_valid_interactable = true
+			interactable = collider
+			interactable.process_raycast_change(true)
+	else:
+		if has_valid_interactable:
+			interactable.process_raycast_change(false)
+		has_valid_interactable = false
+
+func interact():
+	if has_valid_interactable == true:
+		interactable.interact(self)
--- scenes/mask_pickup.gd
+++ scenes/mask_pickup.gd
@@ -1,18 +1,41 @@
 extends Area3D
 
+@export var pickup_offset: Vector3
+
+#stats
 var interactable: bool = true
+
+#references
 var target_node : Node3D 
+var interact_text: Label3D
+var collision_shape: CollisionShape3D
 
 func _ready():
 	interactable = true
+	interact_text = get_node("%InteractText") 
 	target_node = get_node("%MaskPickupSprite") 
+	collision_shape = get_node("collision_shape")
+	
+func show_interact_prompt(show: bool):
+	interact_text.visible = show;
+	
+func process_raycast_change(hit: bool):
+	if hit == true and interactable == true:
+		show_interact_prompt(true)
+	else:
+		show_interact_prompt(false)
 
 # Called when the node enters the scene tree for the first time.
 func interact(interactor: Node3D):
 	print("try interact")
 	if interactable == false:
 		return
-	interactable = false
+	enable_interactions(false)
+	#PICK UP MASK ON PLAYER
 	self.reparent(interactor, true)
-	
-	
+	self.position += pickup_offset
+
+func enable_interactions(enable: bool):
+	interactable = enable
+	collision_shape.visible = enable
+	show_interact_prompt(false)
